62bccdc2964a53856b8ca3116b6d85a3daa21a1a
James Boyden <github@jboy.id.au>
2012-02-04 16:15:16 +1100

    A more accurate model of James.

--- james.py
+++ james.py
@@ -1,11 +1,22 @@
 #!/usr/bin/env python
 
 import random
+from functools import partial
 
 beverages = [ "red bull", "coffee" ]
+languages = [ "python", "PHP", "JavaScript" ]
 
 def caffeinate():
     print "James just sank another %s" % random.choice(beverages)
 
+def upload_ads():
+    print "James just uploaded some ads"
+
+def code(language):
+    print "James just coded in %s" % language
+
+activities = [ caffeinate, upload_ads, partial(code, languages[0]) ]
+
 while 1:
-    caffeinate()
+    random.choice(activities)()
+    
